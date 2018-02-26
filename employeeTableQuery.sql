2e223fa6f696ef4ff5ac983994b396c72ec72ccd
Jim <linchenglong12@gmail.com>
2018-02-26 02:12:23 -0600

    modified column name to suit for the server API

--- employeeTableQuery.sql
+++ employeeTableQuery.sql
@@ -1,2 +1,5 @@
-CREATE TABLE Employee (Record_ID uuid NOT NULL DEFAULT gen_random_uuid(), First_Name Char(10) NOT NULL, Last_Name Char(10) NOT NULL, Empolyee_ID Integer NOT NULL, Active Char(1) CHECK(Active in ('T', 'F')), Role Char(2) CHECK(Role in ('GM', 'SM', 'CH')), Manager uuid, Password Char(10) NOT NULL, Created_On timestamp without time zone NOT NULL DEFAULT now(), Primary Key (Record_ID), Foreign key(Manager) References Employee(Record_ID));
-
+CREATE TABLE Employee (id uuid DEFAULT gen_random_uuid(),
+ First_Name Varchar(10) NOT NULL, Last_Name Varchar(10) NOT NULL, Empolyee_ID Varchar(10) NOT NULL,
+ Active Char(1) CHECK(Active in ('T', 'F')), Role Char(2) CHECK(Role in ('GM', 'SM', 'CH')),
+ Manager uuid, Password Varchar(10) NOT NULL, createdOn timestamp without time zone NOT NULL DEFAULT now(),
+ Primary Key (id), Foreign key(Manager) References Employee(id));
