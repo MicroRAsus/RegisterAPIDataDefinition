79211f07d80e21837adc5fa7353562e3053dcb69
Jim <linchenglong12@gmail.com>
2018-02-28 00:08:23 -0600

    Modified employee table definition to suit for 40 char hashed password

--- employeeTableQuery.sql
+++ employeeTableQuery.sql
@@ -1,5 +1,5 @@
 CREATE TABLE Employee (id uuid DEFAULT gen_random_uuid(),
- First_Name Varchar(10) NOT NULL, Last_Name Varchar(10) NOT NULL, Empolyee_ID Varchar(10) NOT NULL,
+ First_Name Varchar(20) NOT NULL, Last_Name Varchar(20) NOT NULL, Empolyee_ID Varchar(4) NOT NULL,
  Active Char(1) CHECK(Active in ('T', 'F')), Role Char(2) CHECK(Role in ('GM', 'SM', 'CH')),
- Manager uuid, Password Varchar(10) NOT NULL, createdOn timestamp without time zone NOT NULL DEFAULT now(),
+ Manager uuid, Password char(40) NOT NULL, createdOn timestamp without time zone NOT NULL DEFAULT now(),
  Primary Key (id), Foreign key(Manager) References Employee(id));
