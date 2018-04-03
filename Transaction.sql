c2a93127ebade53f6ca366c604b9b1a67eb78418
Jim <linchenglong12@gmail.com>
2018-04-03 06:40:28 -0500

    added new field to Transaction table

--- Transaction.sql
+++ Transaction.sql
@@ -1,3 +1,3 @@
-CREATE TABLE Transactions (recordID int PRIMARY KEY, cashierID Varchar(4) NOT NULL, Amount Decimal(11,2) NOT NULL,
+CREATE TABLE Transactions (id uuid DEFAULT gen_random_uuid(), recordID int, cashierID Varchar(4) NOT NULL, Amount Decimal(11,2) NOT NULL,
  transType Char(1) CHECK(transType in ('S', 'R')), referenceID int, createdOn timestamp without time zone NOT NULL DEFAULT now(),
- Foreign key(referenceID) References Transactions(recordID));
+ PRIMARY KEY(recordID), Foreign key(referenceID) References Transactions(recordID));
